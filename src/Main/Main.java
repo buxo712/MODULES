fb0741287111ed4af144fd053a72b939d448d535
buxo712 <bwenge45@gmail.com>
2025-03-02 12:45:51 -0800

    commit#4
    in the NV, I created a second  instance class  in the main class

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -4,12 +4,10 @@ import Itemmanagement.Item;
 import Userdata.User;
 public class Main {
     public static void main(String[] args) {
-        /
+        // Create an instance of the user class
+        User user = new User("bwenge", 32);
         // create the second instance of the use class
         User user1 = new User("rakuten",19);
-        // Create an instance of the Item class
-        Item item= new Item("iphone13",1200,2024);
-
         // Create an instance of the Item class
         Item item= new Item("iphone13",1200,2024);
 
